fcda0008b614d807a6e69d4cb1f0f1a8465c9558
ssdoz2sk <mybalance22@gmail.com>
2015-10-18 14:23:20 +0800

    Just for Homework

---
+++ HW/install_python3_for_centos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yum groupinstall "Development tools"
+wget http://python.org/ftp/python/3.4.0/Python-3.4.0.tar.xz
+tar xf Python-3.4.0.tar.xz
+cd Python-3.4.0
+./configure --prefix=/usr/local --enable-shared
+make 
+make altinstall
