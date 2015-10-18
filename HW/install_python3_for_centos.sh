66db2e52413cf1549e980661add54f0079921a5c
ssdoz2sk <mybalance22@gmail.com>
2015-10-18 16:07:57 +0800

    Update install_python3_for_centos.sh

--- HW/install_python3_for_centos.sh
+++ HW/install_python3_for_centos.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+wget https://raw.githubusercontent.com/ssdoz2sk/NCYU/master/HW/Setup.dist
 wget http://python.org/ftp/python/3.4.0/Python-3.4.0.tar.xz
 tar -xf Python-3.4.0.tar.xz
+mv -f ./Setup.dist ./Python-3.4.0/Modules/Setup.dist
 cd Python-3.4.0
-./configure --prefix=/usr/local --enable-shared
+./configure --prefix=/usr/local --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib"
 make 
 make altinstall
